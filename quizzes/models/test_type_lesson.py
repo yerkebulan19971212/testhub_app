8626bde933d359de0069791cae24ba45fd93d77a
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2022-07-22 00:27:23 +0600

    related_name testtype_lesson

--- quizzes/models/test_type_lesson.py
+++ quizzes/models/test_type_lesson.py
@@ -8,11 +8,13 @@ from quizzes.models.test_type import TestType
 class TestTypeLesson(TimeStampedModel):
     lesson = models.ForeignKey(
         lesson.Lesson,
+        related_name='test_type_lessons',
         on_delete=models.CASCADE,
         blank=True
     )
     test_type = models.ForeignKey(
         TestType,
+        related_name='test_type_lessons',
         on_delete=models.CASCADE,
         blank=True
     )
