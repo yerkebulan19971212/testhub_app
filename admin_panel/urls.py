296023ab1a9ac5988239bbb212575a34916627a0
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2022-07-26 14:14:19 +0600

    add question view

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-from .views import index
+from .views import index, add_question
 app_name = 'admin_panel'
 urlpatterns = [
     path('index/', index, name='index'),
+    path('add-question/', add_question, name='add_question'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'admin_panel/index.html')
+
+
+def add_question(request):
+    return render(request, 'admin_panel/contents/add_question.html')
