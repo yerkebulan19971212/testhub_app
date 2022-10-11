a480fca6cb6b34867c91b36d8d2846efcac06511
yerkebulan19971212 <erkebulan19971212@gmail.com>
2022-10-11 19:42:43 +0600

    new add question algorithm

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 from .models import (Lesson, LessonGroup, LessonPair, LessonQuestionLevel,
                      Question, QuestionLevel, TestType, TestTypeLesson,
-                     TestTypeLessonGroup, UserVariant, Variant,
-                     VariantQuestion, NumberOfQuestions, FlashCard, CommonQuestion)
+                     TestTypeLessonGroup, UserVariant, Variant, Answer,
+                     VariantQuestion, NumberOfQuestions, FlashCard,
+                     CommonQuestion)
 
 admin.site.register([
     Lesson,
@@ -22,3 +23,8 @@ admin.site.register([
     NumberOfQuestions,
     CommonQuestion
 ])
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('answer', 'correct', 'id', 'question', )
