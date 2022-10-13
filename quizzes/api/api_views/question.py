77e658e45d385b8e6ac1098832d4e1c4dffa139c
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2022-10-13 11:48:36 +0600

    remove auth

--- quizzes/api/api_views/question.py
+++ quizzes/api/api_views/question.py
@@ -5,11 +5,9 @@ from quizzes.api.serializers import QuestionsSerializer
 from quizzes.models import Question
 
 
-
 class QuestionsListView(generics.ListAPIView):
     queryset = Question.objects.all().order_by('-id')[:20]
     serializer_class = QuestionsSerializer
-    permission_classes = (IsAuthenticated,)
 
 
 questions_list = QuestionsListView.as_view()
