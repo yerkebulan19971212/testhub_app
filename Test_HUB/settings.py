a4099fb80cb5ab7ad322fdd00c1ce087dd550362
nura <NuraNK>
2022-10-19 22:11:16 +0600

    add DetailQuiestion field is_favorite

--- Test_HUB/settings.py
+++ Test_HUB/settings.py
@@ -22,7 +22,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
--- quizzes/api/api_views/favorites.py
+++ quizzes/api/api_views/favorites.py
@@ -38,6 +38,8 @@ class ListFavoritesView(generics.ListAPIView):
         return self.queryset.filter(
             favorites__is_favorite=True,
             user=self.request.user
+        ).prefetch_related(
+            'answers'
         )
 
 
