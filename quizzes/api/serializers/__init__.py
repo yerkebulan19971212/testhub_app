ef8c71426c59b959b0767acb9edcc888e409f64b
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2022-10-25 14:25:46 +0600

    finish

--- quizzes/api/serializers/__init__.py
+++ quizzes/api/serializers/__init__.py
@@ -2,7 +2,7 @@ from .favorite import FavoritesSerializer
 from .flash_cards import FlashCardsSerializer
 from .lesson import LessonSerializer, LessonWithTestTypeLessonSerializer
 from .number_of_questions import NumberOfQuestionsSerializer
-from .pass_answer import PassAnswerSerializer
+from .pass_answer import PassAnswerSerializer, FinishByLessonSerializer
 from .question import QuestionsSerializer
 from .quiz_event import QuizEventSerializer
 from .tag import TagListSerializer
