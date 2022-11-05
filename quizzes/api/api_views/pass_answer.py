12596bb7d2fd489e6a1e567312366dacf8d9e504
yerkebulan.abdilkakov <yerkebulan.abdilkakov@fdrive.kz>
2022-11-05 14:44:03 +0600

    -add user_answers

--- quizzes/api/api_views/pass_answer.py
+++ quizzes/api/api_views/pass_answer.py
@@ -2,8 +2,8 @@ from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from quizzes.api.serializers import PassAnswerSerializer, \
-    FinishByLessonSerializer
+from quizzes.api.serializers import (FinishByLessonSerializer,
+                                     PassAnswerSerializer)
 from quizzes.models import PassAnswer, Question, QuizEventQuestion
 
 
