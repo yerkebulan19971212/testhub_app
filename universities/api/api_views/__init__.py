107520926e4d6234d14c47a2bf35e8cb2289fe89
yerkebulan19971212 <erkebulan19971212@gmail.com>
2023-04-14 02:00:46 +0600

    speciality get

--- universities/api/api_views/__init__.py
+++ universities/api/api_views/__init__.py
@@ -1,3 +1,3 @@
 from .universities import (
     country_list, university_list, kazakhstan_university_list,
-    university, university_speciality_list, speciality_list)
+    university, university_speciality_list, speciality_list, speciality)
--- universities/urls.py
+++ universities/urls.py
@@ -2,14 +2,15 @@ from django.urls import include, path
 
 from universities.api.api_views import (
     kazakhstan_university_list, university_list,
-    country_list,speciality_list,
-    university_speciality_list, university)
+    country_list, speciality_list,
+    university_speciality_list, university, speciality)
 
 urlpatterns = [
     path('country-list/', country_list),
     path('university-list/', university_list),
     path('kazakhstan-university-list/', kazakhstan_university_list),
-    path('speciality-university-list/', university_speciality_list),
+    # path('speciality-university-list/', university_speciality_list),
     path('speciality-list/', speciality_list),
+    path('speciality/<int:pk>/', speciality),
     path('university/<int:pk>/', university),
 ]
